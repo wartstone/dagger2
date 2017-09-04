3789d048d37ccdeb209da0029c8be11253bfdcd0
zhengls <774761506@qq.com>
2017-09-04 15:16:40 +0800

    框架: 修改模板Activity(使用注解例子)

--- app/src/main/java/com/vertical/app/module/member/MemberActivity.java
+++ app/src/main/java/com/vertical/app/module/member/MemberActivity.java
@@ -1,37 +1,15 @@
 package com.vertical.app.module.member;
 
-import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
-
 import com.vertical.annotation.AutoWire;
+import com.vertical.annotation.autolayout.AutoLayout;
 import com.vertical.app.R;
 import com.vertical.app.base.BaseCatActivity;
 
-//import butterknife.BindView;
-
 @AutoWire(presenter = MemberPresenter.class, contract = MemberContract.class)
+@AutoLayout(layout = R.layout.activity_member)
 public class MemberActivity extends BaseCatActivity<MemberContract.Presenter> implements MemberContract.View {
     private final String TAG = "MemberActivity";
-
-//    @BindView(R.id.detail_content)
-    TextView mContent;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
-
-//    @Override
-//    protected void inject() {
-//        getActivityComponent().inject(this);
-//    }
-
-    @Override
-    protected int getLayout() {
-        return R.layout.activity_home;
-    }
 
     @Override
     protected void onViewCreated() {
@@ -39,8 +17,12 @@ public class MemberActivity extends BaseCatActivity<MemberContract.Presenter> im
     }
 
     @Override
+    protected void initEventAndData() {
+
+    }
+
+    @Override
     public void showContent() {
-        //mContent.setText("dependency injected");
         Log.d(TAG, "showconent");
     }
 }
