d59aa9a50fc51bbeb2860121049897595abc7135
zhengls <774761506@qq.com>
2017-11-05 12:54:27 +0800

    添加节日fragment

---
+++ app/src/main/java/com/vertical/app/module/work/operation/BirthdayOperationActivity.java
@@ -0,0 +1,70 @@
+package com.vertical.app.module.work.operation;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.vertical.annotation.autolayout.AutoLayout;
+import com.vertical.app.R;
+import com.vertical.app.base.BaseCatActivity;
+import com.vertical.app.module.work.salesvisit.GratitudeVisitFragment;
+import com.vertical.app.module.work.salesvisit.MaintenanceFragment;
+import com.vertical.app.module.work.salesvisit.SatisficationFragment;
+
+import butterknife.BindView;
+
+/**
+ * Created by ls on 11/2/17.
+ */
+
+@AutoLayout(layout = R.layout.activity_salesvisit, title = "销售回访")
+public class BirthdayOperationActivity extends BaseCatActivity {
+    @BindView(R.id.tab_layout)
+    TabLayout mTabLayout;
+    @BindView(R.id.pager)
+    ViewPager mViewPager;
+
+    @Override
+    protected void onViewCreated() {
+        mViewPager.setAdapter(new SectionPagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new TodayBirthdayFragment();
+                case 1:
+                    return new ComingBirthdayFragment();
+                default:
+                    return new TodayBirthdayFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "今日生日";
+                case 1:
+                    return "临近生日";
+                default:
+                    return "今日生日";
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/vertical/app/module/work/operation/ComingHolidayFragment.java
@@ -0,0 +1,10 @@
+package com.vertical.app.module.work.operation;
+
+import com.vertical.app.core.BaseRefreshFragment;
+
+/**
+ * Created by ls on 11/2/17.
+ */
+
+public class ComingHolidayFragment extends BaseRefreshFragment {
+}
